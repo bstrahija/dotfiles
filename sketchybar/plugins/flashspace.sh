ffff8b2b7816ce7aaedf825c2b26ff8ab53486cb
Boris Strahija <bstrahija@gmail.com>
2025-08-22 14:52:43 +0200

    Trying out sketchybar

--- sketchybar/plugins/flashspace.sh
+++ sketchybar/plugins/flashspace.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sketchybar --set $NAME label="$WORKSPACE - $DISPLAY"
+if [ "$1" = "$WORKSPACE" ]; then
+  sketchybar --set $NAME label.color=0xffff0000
+else
+  sketchybar --set $NAME label.color=0xffffffff
+fi
