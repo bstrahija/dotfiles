62027cbc95be1aa972037fb3471c34582cce7002
Boris Strahija <bstrahija@gmail.com>
2025-08-26 01:05:05 +0200

    Test PHP

---
+++ test/test.blade.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Support\Collection;
+
+$test = new Collection();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Test Blade Format</title>
+    </head>
+
+    <body>
+        @if ($a > 10)
+            <p>A is greater than 10</p>
+        @endif
+
+        @php
+            echo "This will be rendered as @php";
+            $bla = new Collection([1, 2, 3]);
+        @endphp
+
+        <div>kljlkj</div>
+    </body>
+</html>
---
+++ test/test.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use function Bla\test;
+
+class App
+{
+    protected string $message = 'Hello, World!';
+
+    public function __construct()
+    {
+        self::stat(1, 'string');
+        $this->message = $this->message . ' from App class!';
+
+        echo $this->message;
+    }
+
+    /**
+     * A test method that returns a string.
+     *
+     * @param  string  $b
+     * @return void
+     */
+    public function test(int $a, $b = null): int
+    {
+        $arr = [
+            'key1'     => 'value1',
+            'key24324' => 'value2',
+            'key3321'  => 'value3',
+            'key4'     => 'value4',
+        ];
+
+        $lelele = 1;
+        $a = 3;
+
+        // Hey hey
+        $class = new class
+        {
+            public function getMessage(): string
+            {
+                return 'This is a message from the anonymous class.';
+            }
+        };
+
+        return 'This is a test method.';
+    }
+
+    public static function stat($hehe)
+    {
+        return $hehe;
+    }
+}
